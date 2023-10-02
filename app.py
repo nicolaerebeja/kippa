905faf4f812c6f95d6b282990ad199d6071fc594
nicolaerebeja <rebejanicolae@gmail.com>
2023-10-02 17:38:19 +0300

    login

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask
+from website import create_app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+# test
